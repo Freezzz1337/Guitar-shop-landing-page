ab693423b136847b40bc1c829ef4d20a792d652e
Artem Samokysh <samokish777bombila@gmail.com>
2023-09-09 17:55:19 +0300

    add guitar model

---
+++ backend/src/main/java/backend/models/Guitar.java
@@ -0,0 +1,37 @@
+package backend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "guitar")
+public class Guitar {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "price")
+    private int price;
+
+    @Column(name = "description", length = 255)
+    private String description;
+
+    @Lob
+    @Column(name = "picture")
+    private byte[] picture;
+}
